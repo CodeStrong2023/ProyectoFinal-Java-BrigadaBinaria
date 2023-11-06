8a81d0aae6df9db695bfeeda77cfe501d9bf0315
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-11-06 12:42:18 -0300

    Modificacion - Clientes ingresados desde el BackEnd

--- Proyecto en Java/src/ProgramaPrincipal.java
+++ Proyecto en Java/src/ProgramaPrincipal.java
@@ -5,25 +5,19 @@ public class ProgramaPrincipal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Cliente> listaClientes = new ArrayList<>();
-    //Creamos clientes para mostrar su información
-    int cantidadInicialClientes = 6;
-        //Objetos(personas) que representan a los clientes
-        Cliente persona1 = new Cliente(46547662, "Angelina", "Pizzolatto", "2604749366", "angelinapizzolatto@gmail.com", 25000, 2500); 
-        persona1.mostrarInformacionCliente();
-        Cliente persona2 = new Cliente(42948039, "Martino", "Sorroche", "2604747856", "martinos@gmail.com", 42000, 4200); 
-        persona2.mostrarInformacionCliente();
-        Cliente persona3 = new Cliente(46546347, "Santiago", "Castro", "2604389433", "santicastro@gmail.com", 13000, 0);
-        persona3.mostrarInformacionCliente();
-        Cliente persona4 = new Cliente(46547662, "Julieta", "Martínez", "2604286544", "julimartinez13@gmail.com", 3000, 0);
-        persona4.mostrarInformacionCliente();
-        Cliente persona5 = new Cliente(46547662, "Fernanda", "López", "2604542755", "lopezfer45@gmail.com", 10000, 1000);
-        persona5.mostrarInformacionCliente();
-        Cliente persona6 = new Cliente(46547662, "Camila", "Gómez", "2604916352", "camigomez@gmail.com", 9900, 0);
-        persona6.mostrarInformacionCliente();
-
-        //Le pedimos al usuario la cantidad de clientes que ingresará
+   //Clientes ingresados desde el backend; 
+        int cantidadInicialClientes = 6; 
+        // Clientes ya cargados por nosotros (son 6 clientes del tipo object)
+            listaClientes.add(new Cliente(42948039, "Zhou", "Yáng", "2604749366", "yangzhou94@gmail.com", 25000));
+            listaClientes.add(new Cliente(42948039, "Martino", "Sorroche", "2604747856", "martinos@gmail.com", 50000));
+            listaClientes.add(new Cliente(46546347, "Franco", "Romero", "2604389433", "santicastro@gmail.com", 10000));
+            listaClientes.add(new Cliente(43765994, "Fernanda", "Torres", "2604542755", "lopezfer45@gmail.com", 5000));
+            listaClientes.add(new Cliente(34564704, "Camila", "Flores", "2604916352", "camigomez@gmail.com", 9900));
+            listaClientes.add(new Cliente(19446237, "Julieta", "Lombardi", "2604286544", "julimartinez13@gmail.com", 3000));
+        System.out.println("REGISTRO DE CLIENTES - TIENDA DE ROPA");
         System.out.println("¿Cuántos clientes adicionales desea ingresar?");
         int cantidadNuevosClientes = scanner.nextInt();
+        scanner.nextLine(); // Consumimos la nueva línea
         // Agregar nuevos clientes
         for (int i = 1; i <= cantidadNuevosClientes; i++) {
             Cliente cliente = Cliente.ingresarCliente(scanner, i + cantidadInicialClientes);
